7a3cf213ab5b7648547f9a77d29df9d04e86f7d3
barthcaron <39792344+barthcaron@users.noreply.github.com>
2019-05-29 16:42:09 +0200

    Update get_GnomAD_MAFs.py

--- NCBoost_scripts/get_GnomAD_MAFs.py
+++ NCBoost_scripts/get_GnomAD_MAFs.py
@@ -20,17 +20,17 @@ def get_num_lines(file_path):
     
 
 def getPopulationL(ac_pop, an_pop):
-  	acL = list(map(float, ac_pop))
-  	# if an_pop is int:
-  	# 	an_pop = [an_pop]
-  	# an = numpy.sum(map(float, an_pop))
-  	an = float(an_pop)
-  	if acL != [] and acL != [None]:
-    		rac = an - numpy.sum(acL)
-    		acL.append(rac)
-    		mac = sorted(acL, reverse=True)[1]
-    		af = mac / an if an != 0 else None
-  	return af 
+    acL = list(map(float, ac_pop))
+    # if an_pop is int:
+    # 	an_pop = [an_pop]
+    # an = numpy.sum(map(float, an_pop))
+    an = float(an_pop)
+    if acL != [] and acL != [None]:
+        rac = an - numpy.sum(acL)
+        acL.append(rac)
+        mac = sorted(acL, reverse=True)[1]
+        af = mac / an if an != 0 else None
+    return af 
 
 def listFromRegion(chrom, start, end, gnomadAF):
     header = "Allele|Consequence|IMPACT|SYMBOL|Gene|Feature_type|Feature|BIOTYPE|EXON|INTRON|HGVSc|HGVSp|cDNA_position|CDS_position|Protein_position|Amino_acids|Codons|Existing_variation|ALLELE_NUM|DISTANCE|STRAND|FLAGS|VARIANT_CLASS|MINIMISED|SYMBOL_SOURCE|HGNC_ID|CANONICAL|TSL|APPRIS|CCDS|ENSP|SWISSPROT|TREMBL|UNIPARC|GENE_PHENO|SIFT|PolyPhen|DOMAINS|HGVS_OFFSET|GMAF|AFR_MAF|AMR_MAF|EAS_MAF|EUR_MAF|SAS_MAF|AA_MAF|EA_MAF|ExAC_MAF|ExAC_Adj_MAF|ExAC_AFR_MAF|ExAC_AMR_MAF|ExAC_EAS_MAF|ExAC_FIN_MAF|ExAC_NFE_MAF|ExAC_OTH_MAF|ExAC_SAS_MAF|CLIN_SIG|SOMATIC|PHENO|PUBMED|MOTIF_NAME|MOTIF_POS|HIGH_INF_POS|MOTIF_SCORE_CHANGE|LoF|LoF_filter|LoF_flags|LoF_info".split('|')
@@ -79,19 +79,19 @@ def listFromRegion(chrom, start, end, gnomadAF):
 gnomad_annotations = ['meanMAF1000G', 'meanMAFGnomAD', 'meanMAF_AFRGnomAD', 'meanMAF_AMRGnomAD', 'meanMAF_ASJGnomAD', 'meanMAF_EASGnomAD', 'meanMAF_FINGnomAD', 'meanMAF_NFEGnomAD', 'meanMAF_OTHGnomAD']
 
 with open(inF, 'r') as f, open(outF, 'w') as o:
-	for index, l in enumerate(tqdm(f, total=get_num_lines(inF))): 
-		if l.startswith('chr'):
-			old_header = l.rstrip().split('\t')
-			standard_header = old_header[0:43]
-			extra_header = old_header[43:len(old_header)]
-			o.write("%s\n" % ('\t'.join(str(x) for x in (standard_header + gnomad_annotations + extra_header))))
-			continue
-		t = l.rstrip().split('\t')
-		chrom = t[0]
-		pos = int(t[1])
-		standard = t[0:43]
-		extra = t[43:len(t)]
-		if chrom != "Y": 
+    for index, l in enumerate(tqdm(f, total=get_num_lines(inF))): 
+        if l.startswith('chr'):
+            old_header = l.rstrip().split('\t')
+            standard_header = old_header[0:43]
+            extra_header = old_header[43:len(old_header)]
+            o.write("%s\n" % ('\t'.join(str(x) for x in (standard_header + gnomad_annotations + extra_header))))
+            continue
+        t = l.rstrip().split('\t')
+        chrom = t[0]
+        pos = int(t[1])
+        standard = t[0:43]
+        extra = t[43:len(t)]
+        if chrom != "Y": 
             gnomadAF = vcf.Reader(filename='NCBoost_features/gnomad.genomes.r2.0.2.sites.chr{}.vcf.bgz'.format(chrom), compressed=True)
             # af of the 1KB region
             # 500nt left part
